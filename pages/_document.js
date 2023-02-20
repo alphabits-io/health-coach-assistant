3abe86656679019fb6883f0aa27876eb5b8f4090
Kaustubh Bhardwaj <kaustubh.bhardwaj86@gmail.com>
2023-02-20 08:32:21 -0500

    analytics script - plausible

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script defer data-domain="prathna.heycoach.me" src="https://plausible.io/js/script.js"></script>
+      </Head>
       <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
       <link rel="shortcut icon" href="/_Logo.png" />
       <body>
